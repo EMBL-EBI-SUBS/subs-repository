c0359beb9a6532b74e8e924d9dfdd5c072f499b6
David Richardson <davidr@ebi.ac.uk>
2017-11-30 21:48:01 +0000

    find a single project for a submission

--- src/main/java/uk/ac/ebi/subs/repository/repos/submittables/ProjectRepository.java
+++ src/main/java/uk/ac/ebi/subs/repository/repos/submittables/ProjectRepository.java
@@ -1,10 +1,20 @@
 package uk.ac.ebi.subs.repository.repos.submittables;
 
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.repository.query.Param;
 import org.springframework.data.rest.core.annotation.RepositoryRestResource;
+import org.springframework.data.rest.core.annotation.RestResource;
+import org.springframework.security.access.method.P;
 import uk.ac.ebi.subs.repository.model.Project;
 import uk.ac.ebi.subs.repository.projections.SubmittableWithStatus;
+import uk.ac.ebi.subs.repository.security.PreAuthorizeSubmissionIdTeamName;
+
+import java.util.List;
 
 @RepositoryRestResource(excerptProjection = SubmittableWithStatus.class)
 public interface ProjectRepository extends SubmittableRepository<Project> {
 
+    @RestResource(exported = true, path = "project-by-submission", rel = "project-by-submission")
+    @PreAuthorizeSubmissionIdTeamName
+    Project findOneBySubmissionId(@P("submissionId") @Param("submissionId") String submissionId);
 }
