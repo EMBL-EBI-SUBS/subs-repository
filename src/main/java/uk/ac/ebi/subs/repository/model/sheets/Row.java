ac87ac9b889b93bc336669705ffbde44ab9281de
David Richardson <davidr@ebi.ac.uk>
2018-02-16 14:35:21 +0000

    remove method causing derserialization problem

--- src/main/java/uk/ac/ebi/subs/repository/model/sheets/Row.java
+++ src/main/java/uk/ac/ebi/subs/repository/model/sheets/Row.java
@@ -24,10 +24,6 @@ public class Row {
         this.document = jsonNode.toString();
     }
 
-    public void setDocument(String json){
-        this.document = json;
-    }
-
     public Row() {
         cells = new LinkedList<>();
     }
