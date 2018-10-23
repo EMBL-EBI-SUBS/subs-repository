4f7849c2b56aa647a5d8403d943a17fe53a49c94
David Richardson <davidr@ebi.ac.uk>
2018-10-23 11:41:31 +0100

    column capture for protocol use / sample use components

---
+++ src/main/java/uk/ac/ebi/subs/repository/model/templates/RefAndAttributeCapture.java
@@ -0,0 +1,111 @@
+package uk.ac.ebi.subs.repository.model.templates;
+
+import lombok.Builder;
+import lombok.Data;
+import lombok.NonNull;
+import lombok.Singular;
+import org.json.JSONArray;
+import org.json.JSONObject;
+
+import java.util.LinkedHashMap;
+import java.util.LinkedList;
+import java.util.List;
+import java.util.Map;
+
+/**
+ * Capture cell values to meet the SampleUse or ProtocolUse structures
+ * These take the form of a reference, and Attributes
+ *
+ * This capture will capture the reference alias, and named attributes (determined through configuration)
+ *
+ * @see uk.ac.ebi.subs.data.component.SampleUse
+ * @see uk.ac.ebi.subs.data.component.ProtocolUse
+ *
+ * @see uk.ac.ebi.subs.data.component.AbstractSubsRef
+ * @see uk.ac.ebi.subs.data.component.Attribute
+ */
+@Data
+@Builder(toBuilder = true)
+public class RefAndAttributeCapture implements Capture {
+
+    @NonNull
+    private String key;
+
+    @NonNull
+    private String refKey;
+
+    private String displayName;
+
+    @Builder.Default
+    private boolean required = false;
+
+    @Singular
+    private Map<String, AttributeCapture> attributeCaptures = new LinkedHashMap<>();
+
+    @Override
+    public int capture(int position, List<String> headers, List<String> values, JSONObject rowDocument) {
+        JSONArray targetArray = JsonUtils.ensureArray(rowDocument, key);
+        JSONObject targetDocument = new JSONObject();
+        targetArray.put(targetDocument);
+
+        RefCapture refCapture = RefCapture.builder()
+                .refKey(refKey)
+                .asList(false)
+                .build();
+
+        position = refCapture.capture(position,headers,values,targetDocument);
+
+        for (Map.Entry<String,AttributeCapture> entry : attributeCaptures.entrySet()){
+            String primaryColumnName = entry.getKey();
+
+            AttributeCapture attributeCapture = entry.getValue();
+            attributeCapture.setDisplayName(primaryColumnName);
+
+            position = attributeCapture.capture(position,headers,values,targetDocument);
+        }
+
+
+        return position;
+    }
+
+    @Override
+    public int map(int position, List<Capture> captures, List<String> headers) {
+
+        this.setCaptureInList(position, captures, headers.get(position));
+
+        ++position;
+
+        for (Map.Entry<String,AttributeCapture> entry : attributeCaptures.entrySet()){
+            String primaryColumnName = entry.getKey();
+
+            AttributeCapture attributeCapture = entry.getValue();
+            attributeCapture.setDisplayName(primaryColumnName);
+
+            position = attributeCapture.map(position,captures,headers);
+        }
+
+
+        return position;
+    }
+
+    @Override
+    public List<String> additionalExpectedColumnHeaders() {
+        List<String> extraHeaders = new LinkedList<>();
+
+        for (Map.Entry<String,AttributeCapture> entry : attributeCaptures.entrySet()){
+            extraHeaders.add(entry.getKey());
+            extraHeaders.addAll(entry.getValue().additionalExpectedColumnHeaders());
+        }
+
+        return extraHeaders;
+    }
+
+    @Override
+    public Capture copy() {
+        return this.toBuilder().build();
+    }
+
+
+
+
+}
--- src/test/java/uk/ac/ebi/subs/model/templates/RefAndAttributeCaptureTest.java
+++ src/test/java/uk/ac/ebi/subs/model/templates/RefAndAttributeCaptureTest.java
@@ -1,26 +1,156 @@
 package uk.ac.ebi.subs.model.templates;
 
+import org.json.JSONObject;
+import org.junit.Assert;
 import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
-import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+import org.junit.runners.JUnit4;
+import org.skyscreamer.jsonassert.JSONAssert;
+import uk.ac.ebi.subs.repository.model.templates.AttributeCapture;
+import uk.ac.ebi.subs.repository.model.templates.Capture;
+import uk.ac.ebi.subs.repository.model.templates.NoOpCapture;
+import uk.ac.ebi.subs.repository.model.templates.RefAndAttributeCapture;
 
-@RunWith(SpringJUnit4ClassRunner.class)
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.List;
+
+@RunWith(JUnit4.class)
 public class RefAndAttributeCaptureTest {
 
+    RefAndAttributeCapture capture;
+
+    List<String> headers;
+    List<String> values;
+
     @Before
-    public void buildUp(){
+    public void buildUp() {
+        this.capture = RefAndAttributeCapture
+                .builder()
+                .key("protocolUses")
+                .refKey("protocolRef")
+                .required(true)
+                .attributeCapture(
+                        "Post Extraction",
+                        AttributeCapture.builder()
+                                .allowTerms(true)
+                                .allowUnits(false)
+                                .required(false)
+                                .build()
+                )
+                .attributeCapture(
+                        "Extract Name",
+                        AttributeCapture.builder()
+                                .allowTerms(false)
+                                .allowUnits(false)
+                                .required(true)
+                                .build()
+                )
+                .attributeCapture(
+                        "Extract time",
+                        AttributeCapture.builder()
+                                .allowTerms(false)
+                                .allowUnits(true)
+                                .required(false)
+                                .build()
+                )
+                .build();
+
+        this.headers = Arrays.asList(
+                "Extraction Protocol Alias",
+                "Post Extraction",
+                "terms",
+                "Extract Name",
+                "Extract time",
+                "units"
+        );
+
+        this.values = Arrays.asList(
+                "myProtocolAlias",
+                "95:5 (v/v) water:acetonitrile and 0.05 µg/ml deuterated biotin",
+                "http://www.ebi.ac.uk/efo/EFO_0000494",
+                "extract_47",
+                "14.3",
+                "hour"
+        );
+    }
+
+    @Test
+    public void testMapping() {
+        int startPosition = 0;
+        List<Capture> captures = new ArrayList<>(Collections.nCopies(headers.size(), null));
+
+        int endPosition = capture.map(startPosition, captures, this.headers);
+
+        Assert.assertEquals(6, endPosition);
+
+        List<Capture> expectedCaptures = Arrays.asList(
+                capture,
+                capture.getAttributeCaptures().get(headers.get(1)),
+                NoOpCapture.builder().displayName(headers.get(2)).build(),
+                capture.getAttributeCaptures().get(headers.get(3)),
+                capture.getAttributeCaptures().get(headers.get(4)),
+                NoOpCapture.builder().displayName(headers.get(5)).build()
+        );
+
+        Assert.assertEquals(expectedCaptures, captures);
 
     }
 
     @Test
-    public void testMapping(){
+    public void testCapture() {
+
+        JSONObject jsonExpected = new JSONObject("{\n" +
+                "  \"protocolUses\": [\n" +
+                "    {\n" +
+                "      \"protocolRef\": {\n" +
+                "        \"alias\": \"myProtocolAlias\"\n" +
+                "      },\n" +
+                "      \"attributes\": {\n" +
+                "        \"Post Extraction\": [\n" +
+                "          {\n" +
+                "            \"value\": \"95:5 (v/v) water:acetonitrile and 0.05 µg/ml deuterated biotin\",\n" +
+                "            \"terms\": [\n" +
+                "              {\n" +
+                "                \"url\": \"http://www.ebi.ac.uk/efo/EFO_0000494\"\n" +
+                "              }\n" +
+                "            ]\n" +
+                "          }\n" +
+                "        ],\n" +
+                "        \"Extract Name\": [\n" +
+                "          {\n" +
+                "            \"value\": \"extract_47\"\n" +
+                "          }\n" +
+                "        ],\n" +
+                "        \"Extract time\": [\n" +
+                "          {\n" +
+                "            \"value\": \"14.3\",\n" +
+                "            \"units\": \"hour\"\n" +
+                "          }\n" +
+                "        ]\n" +
+                "      }\n" +
+                "    }\n" +
+                "  ]\n" +
+                "}");
+
+        JSONObject jsonActual = new JSONObject();
+
+        int startPosition = 0;
+
+        int endPosition = capture.capture(startPosition, headers, values, jsonActual);
+
+        JSONAssert.assertEquals(jsonExpected, jsonActual, true);
+        Assert.assertEquals(6, endPosition);
 
     }
 
     @Test
-    public void testCapture(){
+    public void testExpectedColumns() {
+        List<String> actualExtraColumnHeaders = capture.additionalExpectedColumnHeaders();
 
+        Assert.assertEquals(headers.subList(1, 6), actualExtraColumnHeaders);
     }
 
 
