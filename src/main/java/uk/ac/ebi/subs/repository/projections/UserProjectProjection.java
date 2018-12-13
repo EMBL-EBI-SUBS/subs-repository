f7b636480bbfec903c60bafd1322aeb657c032eb
Karoly Erdos <karoly@ebi.ac.uk>
2018-12-13 16:30:02 +0000

    Add release date accessor to UserProjectProjection interface

--- src/main/java/uk/ac/ebi/subs/repository/projections/UserProjectProjection.java
+++ src/main/java/uk/ac/ebi/subs/repository/projections/UserProjectProjection.java
@@ -3,6 +3,9 @@ package uk.ac.ebi.subs.repository.projections;
 import org.springframework.data.rest.core.config.Projection;
 import uk.ac.ebi.subs.repository.model.Project;
 
+import java.time.LocalDate;
+
 @Projection(name = "userProjectProjection",types = {Project.class })
 public interface UserProjectProjection extends SubmittableWithStatus {
+    LocalDate getReleaseDate();
 }
